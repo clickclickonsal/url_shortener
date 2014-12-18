5ec19d6b54ff18d29171eb2fe7cd38a2ea8d2acb
ClickClickkOnSal <s.hernandez5400@gmail.com>
2014-12-18 12:58:29 -0500

    rolledback migration to fix attribute name in schema

--- db/migrate/20141218164315_create_shrink_url.rb
+++ db/migrate/20141218164315_create_shrink_url.rb
@@ -1,7 +1,7 @@
 class CreateShrinkUrl < ActiveRecord::Migration
   def change
     create_table :shrink_urls do |t|
-      t.string :orginal_url
+      t.string :original_url
       t.string :short_url
     end
   end
